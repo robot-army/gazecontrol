7f6dc360a7d288296e7e0f17001f979dc85ee089
Ryan White <ry.white@gmail.com>
2017-10-07 00:37:11 +0100

    Added updated LED driver arduino code

--- Arduino_LEDs/Arduino_LEDs.ino
+++ Arduino_LEDs/Arduino_LEDs.ino
@@ -2,125 +2,47 @@
 #include <string.h>
 
 // CHANGE this number for each fiducial
-#define FIDUCIAL_ID 3
 
 #define PIN 6
-#define NUMPIXELS 28
-#define ARRAYSIZE 38
-#define MAXWIDTH 4
-#define MINWIDTH 1
-#define MAXDIST 1000
+#define MAXLEDS 56
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_RGB + NEO_KHZ800);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(MAXLEDS, PIN, NEO_RGB + NEO_KHZ800);
 
-  
-  int b[ARRAYSIZE];
-  int c[ARRAYSIZE];
-  int d[ARRAYSIZE];
-
+String inputString ="";
+boolean stringComplete = false;
 
 void setup() {
   Serial.begin(115200);
   pixels.begin();
-  
-    //lazy array initialization - fix hacked bounds
-  for(int i=0; i<ARRAYSIZE; i++) {
-    d[i] = c[i] = b[i] = 0;
+  inputString.reserve((3*MAXLEDS)+3);
+}
+
+void loop() {  
+  if (stringComplete) {
+    
+ //   Serial.println(inputString);
+
+    int ledindex = 0;
+    for(int i=0; i<(inputString.length()-1); i=i+3) {
+      int R = (inputString.charAt(i)-'0')*28;
+      int G = (inputString.charAt(i+1)-'0')*28;
+      int B = (inputString.charAt(i+2)-'0')*28;
+      pixels.setPixelColor(ledindex++,R,G,B);
+    }
+
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    inputString = "";
+    stringComplete = false;
+    
   }
 }
 
-void loop() {
-  int angle = 0;
-  int distance = 1000;
-  int centreled;
-  String marker_id;
-  float dist;
-  String  inputstring;
-  String dist_string;
-  String angle_string;
-  int refresh=1;
-
-//these for loops are for testing only, they'll be replaced with a Serial.read() when connected to the other thing
-//
-//for(distance=1000; distance > 50; distance=distance-100){
-//  for(angle=0;angle<360;angle=angle+10){
-
-do {
-  
-while (Serial.available()){
-  inputstring = Serial.readStringUntil('A');
-//  refresh = 1;
+void serialEvent() {
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    inputString += inChar;  
+    if(inChar == '\n') {
+      stringComplete = true;
+    }
+  }
 }
-
-
-    marker_id = inputstring.substring(0,2);
-    dist_string = inputstring.substring(2,6);
-    angle_string = inputstring.substring(6,10);
-    angle = angle_string.toInt();
-    distance = dist_string.toInt();
-
-} while(marker_id.toInt() != FIDUCIAL_ID);
-    
-if(distance==0){
-  distance = 1000;
-}
-    //lazy array initialization - fix hacked bounds
-     for(int i=0; i<ARRAYSIZE; i++) {
-    d[i] = c[i] = b[i] = 0;
-  }
-
-//  Serial.print("Distance: ");
-//  Serial.print(distance);
-//  Serial.print(" Angle: ");
-//  Serial.print(angle);
-//  Serial.print(" Refresh: ");
-//  Serial.println(refresh);
-    
-  centreled = map(angle,0,360,0,NUMPIXELS);
-
-  delay(40);
-  
-  dist = map(distance,0,MAXDIST,MAXWIDTH,MINWIDTH);
-
-
-
- 
-    //make the ramp one-sided
-    for(int i=0; i<(dist+1); i++) {
-      b[i] = 255*float(i/dist);
-      if(distance==1000){
-        b[i] = 0;
-      }
-    }
-
-    
-
-    //make the ramp two sided
-    for(int i=0; i<(2*dist)+1; i++) {
-      int disti = dist-(i-dist);
-      if(i<dist) c[i] = b[i];
-      if(i>=dist & i<(2*dist)+1) c[i] = b[disti];
-      if(i>=(2*dist)+1) c[i] = 0; 
-    }
-
-
-    //centre the ramp around the right pixel and set the pixel colour
-    for(int i=0; i<NUMPIXELS; i++) {
-      int index = (centreled-i+int(dist)) % (NUMPIXELS);
-      pixels.setPixelColor(i,0,c[index],c[index]);    
-    }
-
-    if(angle==9999){
-      for(int i=0; i<NUMPIXELS; i++) {
-        pixels.setPixelColor(i,0,255,0);
-      }
-    }
-
-  if(refresh == 1){
-          pixels.show(); // This sends the updated pixel color to the hardware.
-//      refresh = 0;
-  }
-    
-  }
-//}
-//}
